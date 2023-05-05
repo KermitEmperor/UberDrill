3d243466e9f8834901993ba9a74ed0461a1874fd
Emperor Kermit <74988196+KermitEmperor@users.noreply.github.com>
2023-05-05 12:17:57 +0200

    Added hydrofluoric acid

--- UberDrill_0.0.1/prototypes/fluids.lua
+++ UberDrill_0.0.1/prototypes/fluids.lua
@@ -14,3 +14,20 @@ data:extend({
     order = "a[fluid]-b[fluoroantimonic-acid]"
   }
 })
+
+data:extend({
+  {
+    type = "fluid",
+    name = "hydrofluoric-acid",
+    default_temperature = 25,
+    heat_capacity = "0.1KJ",
+    base_color = {r = 0.604, g = 0.588, b = 0.4},
+    flow_color = {r = 0.604, g = 0.588, b = 0.4},
+    max_temperature = 100,
+    icon = "__UberDrill__/graphics/icons/hydrofluoric-acid.png",
+    icon_size = 64,
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    order = "a[fluid]-b[fluoroantimonic-acid]"
+  }
+})
