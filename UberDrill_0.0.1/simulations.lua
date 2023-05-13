fdb83df33215402f258d0b9f0ccc0dbc9f5f75bd
Emperor Kermit <74988196+KermitEmperor@users.noreply.github.com>
2023-05-13 17:00:01 +0200

    added SE simulation compability

--- UberDrill_0.0.1/simulations.lua
+++ UberDrill_0.0.1/simulations.lua
@@ -1,16 +1,22 @@
 local main_menu_simulations = data.raw["utility-constants"]["default"].main_menu_simulations
 
+local save = "__UberDrill__/simulation/Simulation.zip"
+local simTime = 600
+if mods["space-exploration"] then
+    save = "__UberDrill__/simulation/SimulationSE.zip"
+    simTime = 1200
+end
+
 main_menu_simulations.uber_drill = {
     checkboard = false,
-    save = "__UberDrill__/simulation/Simulation.zip",
-    length = 600,
-    init =
-    [[
-      local logo = game.surfaces.nauvis.find_entities_filtered{name = "factorio-logo-11tiles", limit = 1}[1]
-      game.camera_position = {logo.position.x, logo.position.y+6}
-      game.camera_zoom = 1.5
-      game.tick_paused = false
-      game.surfaces.nauvis.daytime = 0.5
+    save = save,
+    length = simTime,
+    init = [[
+        local logo = game.surfaces.nauvis.find_entities_filtered{name = "factorio-logo-11tiles", limit = 1}[1]
+        game.camera_position = {logo.position.x, logo.position.y+6}
+        game.camera_zoom = 1.5
+        game.tick_paused = false
+        game.surfaces.nauvis.daytime = 0.5
     ]],
     update =
     [[
